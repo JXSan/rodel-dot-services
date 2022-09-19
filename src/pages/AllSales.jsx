0d813992845ff84e635a4e1a119e88c4254961c3
Jon <jonathansanchez1995@gmail.com>
2022-09-18 17:44:26 -0700

    Included order status in all sales

--- src/pages/AllSales.jsx
+++ src/pages/AllSales.jsx
@@ -84,6 +84,11 @@ const AllSales = () => {
             customer_name: transaction?.session?.customer_details?.name,
             customer_email: transaction?.session?.customer_details?.email,
             status: transaction?.status,
+            confirmationNumber: transaction?.confirmationNumber,
+            companyId: transaction?.companyId,
+            order_status: transaction?.confirmationNumber
+              ? "Complete"
+              : "Not Complete",
           };
         });
       setRows(allRows);
@@ -182,83 +187,5 @@ const AllSales = () => {
       </div>
     </div>
   );
-
-  // return (
-  //   <div className="w-full h-full flex items-center justify-center flex-col">
-  //     <div className="stats shadow flex">
-  //       <div className="stat">
-  //         <div className="stat-figure text-secondary">
-  //           <svg
-  //             xmlns="http://www.w3.org/2000/svg"
-  //             fill="none"
-  //             viewBox="0 0 24 24"
-  //             className="inline-block w-8 h-8 stroke-current"
-  //           >
-  //             <path
-  //               strokeLinecap="round"
-  //               strokeLinejoin="round"
-  //               strokeWidth="2"
-  //               d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-  //             ></path>
-  //           </svg>
-  //         </div>
-  //         <div className="stat-title">Todays Total Sales</div>
-  //         <div className="stat-value">$323.32</div>
-  //         <div className="stat-desc">2 Sales Made | September 15, 2022</div>
-  //       </div>
-  //       <div className="stat">
-  //         <div className="stat-figure text-secondary">
-  //           <svg
-  //             xmlns="http://www.w3.org/2000/svg"
-  //             fill="none"
-  //             viewBox="0 0 24 24"
-  //             className="inline-block w-8 h-8 stroke-current"
-  //           >
-  //             <path
-  //               strokeLinecap="round"
-  //               strokeLinejoin="round"
-  //               strokeWidth="2"
-  //               d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-  //             ></path>
-  //           </svg>
-  //         </div>
-  //         <div className="stat-title">This Weeks Total Sales</div>
-  //         <div className="stat-value">$323.32</div>
-  //         <div className="stat-desc">2 Sales Made | September 15, 2022</div>
-  //       </div>
-  //       <div className="stat">
-  //         <div className="stat-figure text-secondary">
-  //           <svg
-  //             xmlns="http://www.w3.org/2000/svg"
-  //             fill="none"
-  //             viewBox="0 0 24 24"
-  //             className="inline-block w-8 h-8 stroke-current"
-  //           >
-  //             <path
-  //               strokeLinecap="round"
-  //               strokeLinejoin="round"
-  //               strokeWidth="2"
-  //               d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-  //             ></path>
-  //           </svg>
-  //         </div>
-  //         <div className="stat-title">This Months Total Sales</div>
-  //         <div className="stat-value">$323.32</div>
-  //         <div className="stat-desc">2 Sales Made | September 15, 2022</div>
-  //       </div>
-  //     </div>
-  //     <div className="w-full  px-4 py-2 rounded-lg flex flex-col">
-  //       <div className="w-full h-96 flex">
-  //         <DataGrid
-  //           className="bg-black/2 rounded-md border border-gray-400 drop-shadow-sm"
-  //           rows={rows}
-  //           columns={columns}
-  //           pageSize={15}
-  //         />
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
-
 export default AllSales;
